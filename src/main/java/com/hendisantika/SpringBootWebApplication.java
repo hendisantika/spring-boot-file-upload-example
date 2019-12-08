1457340aa6ed97107de3e7952006b10fc48b9e3d
Hendi Santika <hendisantika@gmail.com>
2019-12-08 07:10:19 +0700

    #17 - Upgraded into Spring Boot 2.2.0.RELEASE version

--- src/main/java/com/hendisantika/SpringBootWebApplication.java
+++ src/main/java/com/hendisantika/SpringBootWebApplication.java
@@ -1,11 +1,7 @@
 package com.hendisantika;
 
-import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 
 //https://www.agilegroup.co.jp/technote/springboot-fileupload-error-handling.html
 @SpringBootApplication
@@ -19,20 +15,38 @@ public class SpringBootWebApplication {
 
     //Tomcat large file upload connection reset
     //http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
-    @Bean
-    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+//    @Bean
+//    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+//
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+//
+//        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+//            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+//                //-1 means unlimited
+//                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+//            }
+//        });
+//
+//        return tomcat;
+//
+//    }
 
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-
-        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
-            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
-                //-1 means unlimited
-                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
-            }
-        });
-
-        return tomcat;
-
-    }
+//    @Bean
+//    public TomcatServletWebServerFactory containerFactory() {
+//        return new TomcatServletWebServerFactory() {
+//            protected void customizeConnector(Connector connector) {
+//                int maxSize = 50000000;
+//                super.customizeConnector(connector);
+//                connector.setMaxPostSize(maxSize);
+//                connector.setMaxSavePostSize(maxSize);
+//                if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
+//
+//                    ((AbstractHttp11Protocol <?>) connector.getProtocolHandler()).setMaxSwallowSize(maxSize);
+//                    logger.info("Set MaxSwallowSize "+ maxSize);
+//                }
+//            }
+//        };
+//
+//    }
 
 }
